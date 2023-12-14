3851dad47cf5e735a8bc511ce95ef5e09e3b8b55
samarthrn <samarthnathile7670@gmail.com>
2023-12-14 15:27:21 +0530

    datatypes

---
+++ Basics-01/02_datatypes.js
@@ -0,0 +1,13 @@
+// Numbers
+let age =20
+
+// String
+let name ="Samarth"
+
+//Boolean
+let criminal ="false"
+
+//Objects
+const details ={firstname:"Jhonny",lastname:"Sins"}
+
+console.log(details);
