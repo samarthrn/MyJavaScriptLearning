fcfb99a099e5bd11943f91fd267dcfa62dd540f1
samarthrn <samarthnathile7670@gmail.com>
2023-12-14 12:55:46 +0530

    Learned Difference Between -[Let & Var]  |  Const can't be updated |  Console.table

---
+++ Basics-01/01.js
@@ -0,0 +1,17 @@
+const accNum = 12348888
+let accEmail = "samarth@gmail.com"
+var accPass  = "Pass@123"
+let accLogin; 
+
+//accNum = 2 // got to know about const cannot be updated and throws error when doing so.
+
+/* 
+    always use let over var to avoid issues for variable updation
+    also we can print a variable which does not have any value, JS gives output of such variable as "UNDEFINED"
+    also i learned how to setup git and github using vscode.
+*/
+
+
+console.log(accNum);
+console.log(accEmail);
+console.table([accEmail,accNum,accPass,accLogin]);
