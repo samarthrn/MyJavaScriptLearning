56773ae66058cbd936f9d9bd03a7efcdb1b4181f
Samarth <107809095+samarthrn@users.noreply.github.com>
2023-12-18 11:00:54 +0000

    data types

---
+++ Basics-01/03_datatypesSummary.js
@@ -0,0 +1,14 @@
+/* 
+Data types are divided into two categories based upon how they are stored in memory and how can we access them: 
+
+    1) Primitive (Call by value i.e value is given after coyping)
+        String, Number, Boolean, null, undefined, Symbol, BigInt
+
+    2) Non-Primitive (Reference type)
+    the values whose reference is directly given in the memory
+        Array, Object, Functions
+
+*/
+
+const Age    =20
+const Marks = 
